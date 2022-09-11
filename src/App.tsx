9ed1b8edb855af601e64a96057ca07c07d019a4e
Mourad <mouradface1998@gmail.com>
2022-09-11 10:44:49 +0100

    Issue with setting up absolute paths @ or ~

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "~/assets/svg/logo.svg";
 
 function App() {
   return (
@@ -10,12 +8,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
