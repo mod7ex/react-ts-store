37b51af49aff1da37935d037dba996df0b88b9d3
Mourad <mouradface1998@gmail.com>
2022-09-18 08:24:33 +0100

    issue with scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 // import useToast from "~/hooks/useToast";
 import ToastsList from "~/partials/ToastsList";
 import "~/index.scss";
+import { useState } from "react";
+import { Transition } from "react-transition-group";
 
 function App() {
   // const toaster = useToast();
@@ -9,10 +11,33 @@ function App() {
   //   toaster.success({ title: "Title", content: "some ipsom data" });
   // };
 
+  const [show, setShow] = useState(true);
+
   return (
     <>
       <ToastsList />
       {/* <button onClick={() => handelClick()}>click</button> */}
+
+      <h1>
+        <button onClick={() => setShow((v) => !v)}>toggle</button>
+      </h1>
+
+      <Transition timeout={300}>
+        <div style={{ backgroundColor: "blue", padding: "1rem" }}>
+          {show ? (
+            <div style={{ backgroundColor: "green", maxWidth: "20rem", margin: "0 auto" }}>
+              Lorem ipsum dolor sit amet consectetur.
+              <br />
+              Lorem ipsum dolor sit amet consectetur.
+              <br />
+              Lorem ipsum dolor sit amet consectetur.
+              <br />
+              Lorem ipsum dolor sit amet consectetur.
+              <br />
+            </div>
+          ) : null}
+        </div>
+      </Transition>
     </>
   );
 }
